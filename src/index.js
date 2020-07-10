c4bc61a9106f17998d392443a56ff55dc8a4075a
TTPOCTOLikes <dyagovets.vova2@gmail.com>
2020-07-10 12:49:27 +0300

    initial commit.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from "redux-thunk";
+import {Provider} from 'react-redux';
+import {App} from './App';
+import {rootReducer} from "./redux/rootReducer";
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    )
+);
+
+const app = (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
